381d1c4a98c25f0af153b5eeebbc45132b7a712e
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-13 16:25:39 -0300

    update LiveTv

--- pages/LiveTv.js
+++ pages/LiveTv.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Header from '../components/Header';
 import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView, ScrollView } from 'react-native-web';
 
 export default function LiveTv() {
     const navigation = useNavigation();
     return (
-        <View style={styles.container}>
+        <ScrollView>
+        <SafeAreaView style={styles.container}>
             <Header />
-            <Text>Tela Live</Text>
-        </View>
+            <Text style={styles.title}>VÍDEOS</Text>
+            <TouchableOpacity style={styles.input}>Pesquisar Vídeos</TouchableOpacity>
+        </SafeAreaView>
+        </ScrollView>
     );
 }
 
@@ -19,4 +23,21 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         width: '100%',
     },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000000',
+        padding: 20,
+        marginTop: 10,
+    },
+    input: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 8,
+        margin: 16,
+        backgroundColor: '#acacac',
+        color: '#5a5a5a',
+    }
 });
