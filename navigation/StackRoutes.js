7795fd84da73608ae84a0dc771fb7e08f9b6eec9
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-13 14:02:10 -0300

    I and Luiza: update StackRoutes

--- navigation/StackRoutes.js
+++ navigation/StackRoutes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
-import TabRoutes from './TabRoutes';
+import DrawerRoutes from './DrawerRoutes';
 
 const Stack = createStackNavigator();
 
 export default function StackRoutes() {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="TabRoutes" component={TabRoutes} />
+            <Stack.Screen name="Recentes" component={DrawerRoutes} />
+            <Stack.Screen name="Casos Criminais" component={DrawerRoutes} />
+            <Stack.Screen name="Podcast" component={DrawerRoutes} />
+            <Stack.Screen name="Quiz" component={DrawerRoutes} />
         </Stack.Navigator>
     );
 }
