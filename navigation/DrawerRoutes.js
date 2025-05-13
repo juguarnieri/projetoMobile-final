268ca6920717b4cc09d09745f4e2997820b315aa
Luiza <luizaschettinii@gmail.com>
2025-05-13 13:44:22 -0300

    feat: add drawer route

---
+++ navigation/DrawerRoutes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import TabRoutes from "./TabRoutes";
+import Recentes from "../pages/Recentes";
+import CasosCriminais from "../pages/CasosCriminais";
+import Podcast from "../pages/Podcast";
+import Quiz from "../pages/Quiz";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerRoutes() {
+    return (
+    <Drawer.Navigator>
+    screenOptions={{
+        drawerActiveTintColor: 'red',
+        drawerInactiveTintColor: 'gray',
+        drawerStyle: { backgroundColor: '#fff', width: 250 },
+        headerShown: true,
+    }}
+    <Drawer.Screen name="Home" component={TabRoutes} />
+    <Drawer.Screen name="Recentes" component={Recentes} />
+    <Drawer.Screen name="Casos Criminais" component={CasosCriminais} />
+    <Drawer.Screen name="Podcast" component={Podcast} />
+    <Drawer.Screen name="Quiz" component={Quiz} />
+
+    </Drawer.Navigator>
+    );
+}
