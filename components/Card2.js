ecffa081389c937ffa680c4e3394ba485085e736
anna leme <annaleme.tds1.senai@gmail.com>
2025-05-28 23:40:49 -0300

    refactor: page podcast

--- components/Card2.js
+++ components/Card2.js
@@ -1,50 +1,31 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
-export default function Card2({ imageUri, title, description }) {
-    return (
-        <View style={styles.container}>
-                    <View style={styles.row}>
-                        <Image 
-                            source={{ uri: imageUri }}
-                            style={styles.photo}
-                        />
-                        <View>
-                            <Text style={styles.title}>{title}</Text>
-                            <Text style={styles.description}>{description}</Text>
-                        </View>
-                    </View>
-                </View>
-    );
+export default function Card2({ imageUri, title }) {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: imageUri }} style={styles.image} />
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: '43.5%',
-        marginLeft: '7%',
-        marginTop: 10,
-    },
-    title: {
-        color: 'black',
-        fontSize: 16,
-        fontWeight: '600',
-        marginTop: 10,
-    },
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 20,
-
-    },
-    photo: {
-        width: '100%',
-        height: 100,
-        marginRight: 10,
-    },
-    description: {
-        color: 'gray',
-        fontSize: 14,
-        marginTop: 5,
-        marginLeft: 2,
-    },
+  container: {
+    width: 180,
+    marginLeft: 15,
+    marginTop: 10,
+  },
+  image: {
+    width: '100%',
+    height: 140,
+    borderRadius: 8,
+  },
+  title: {
+    marginTop: 8,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
 });
